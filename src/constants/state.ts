659ea3a66a96dc7a180c01fa5d61364eb74cc73a
Mark <mark@remarkablemark.org>
2025-05-12 23:09:27 -0400

    chore(gameobjects): add hit state to human

--- src/constants/state.ts
+++ src/constants/state.ts
@@ -1,5 +1,6 @@
 export enum State {
   Attack = 'Attack',
+  Hit = 'Hit',
   Idle = 'Idle',
   Move = 'Move',
 }
--- src/gameobjects/human.ts
+++ src/gameobjects/human.ts
@@ -32,25 +32,39 @@ export function addHuman(x: number, y: number) {
     }
   })
 
+  human.onStateUpdate(State.Idle, () => {
+    if (shouldMove()) {
+      human.enterState(State.Move)
+    }
+  })
+
   human.onStateEnter(State.Move, () => {
     human.play(Animation.Run, { loop: true })
   })
 
   human.onStateUpdate(State.Move, () => {
-    if (!shouldMove()) {
+    if (shouldMove()) {
+      const direction = player.pos.sub(human.pos).unit()
+      human.move(direction.scale(-speed))
+    } else {
       human.enterState(State.Idle)
     }
-    const direction = player.pos.sub(human.pos).unit()
-    human.move(direction.scale(-speed))
+  })
+
+  human.onStateEnter(State.Hit, () => {
+    human.play(Animation.Hit, {
+      onEnd: () => human.enterState(State.Idle),
+    })
   })
 
   human.onCollide(Tag.Zombie, () => {
+    human.enterState(State.Hit)
     human.hurt(1)
   })
 
   // @ts-expect-error This expression is not callable. Type 'Collision' has no call signatures.
   human.onCollideUpdate(Tag.Zombie, () => {
-    human.hurt(0.1)
+    human.hurt(0)
   })
 
   return human
