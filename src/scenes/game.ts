894243d0a0ed4b96b4e180bd6c8bb6b91bbd43be
Mark <mark@remarkablemark.org>
2025-05-13 21:29:37 -0400

    refactor(levels): modularize map from game

--- src/scenes/game.ts
+++ src/scenes/game.ts
@@ -1,41 +1,32 @@
 import { Scene, Size, Sprite } from '../constants'
 import { addHuman, addPlayer } from '../gameobjects'
+import { levels } from '../levels'
 
-scene(Scene.Game, () => {
-  addLevel(
-    [
-      '............',
-      '.        H .',
-      '.          .',
-      '.  P       .',
-      '.    H     .',
-      '.          .',
-      '.          .',
-      '.          .',
-      '.     H    .',
-      '.          .',
-      '............',
-    ],
-    {
-      tileWidth: Size.Tile,
-      tileHeight: Size.Tile,
-      tiles: {
-        '.': () => [
-          rect(Size.Tile, Size.Tile),
-          area(),
-          body({ isStatic: true }),
-          opacity(0),
-        ],
-        ' ': () => [sprite(Sprite.Floor)],
-        P: (pos) => {
-          addPlayer(pos.x * Size.Tile, pos.y * Size.Tile)
-          return [sprite(Sprite.Floor)]
-        },
-        H: (pos) => {
-          addHuman(pos.x * Size.Tile, pos.y * Size.Tile)
-          return [sprite(Sprite.Floor)]
-        },
+scene(Scene.Game, (level = 0) => {
+  const { map } = levels[level]
+
+  addLevel(map, {
+    tileWidth: Size.Tile,
+    tileHeight: Size.Tile,
+    tiles: {
+      ' ': () => [sprite(Sprite.Floor)],
+
+      '.': () => [
+        rect(Size.Tile, Size.Tile),
+        area(),
+        body({ isStatic: true }),
+        opacity(0),
+      ],
+
+      P: (pos) => {
+        addPlayer(pos.x * Size.Tile, pos.y * Size.Tile)
+        return [sprite(Sprite.Floor)]
+      },
+
+      H: (pos) => {
+        addHuman(pos.x * Size.Tile, pos.y * Size.Tile)
+        return [sprite(Sprite.Floor)]
       },
     },
-  )
+  })
 })
