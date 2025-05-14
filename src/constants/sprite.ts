20687a629733632fdff901d2429ec63d1acb74e0
Mark <mark@remarkablemark.org>
2025-05-14 18:30:43 -0400

    chore(sprites): load zombies 2, 3, and 4

--- src/constants/sprite.ts
+++ src/constants/sprite.ts
@@ -2,4 +2,7 @@ export enum Sprite {
   Floor = 'Floor',
   Human1 = 'Human1',
   Zombie1 = 'Zombie1',
+  Zombie2 = 'Zombie2',
+  Zombie3 = 'Zombie3',
+  Zombie4 = 'Zombie4',
 }
--- src/scenes/preload.ts
+++ src/scenes/preload.ts
@@ -2,29 +2,44 @@ import { Animation, Scene, Sprite } from '../constants'
 import { startLevel } from '../levels'
 
 scene(Scene.Preload, async () => {
-  loadSprite(Sprite.Zombie1, 'sprites/zombies/zombie1.png', {
-    sliceX: 8,
-    sliceY: 6,
-    anims: {
-      [Animation.Idle]: { from: 0, to: 5 },
-      [Animation.Run]: { from: 8, to: 15 },
-      [Animation.Knocked]: { from: 16, to: 21 },
-      [Animation.Hit]: { from: 24, to: 26 },
-      [Animation.Death]: { from: 32, to: 39 },
-      [Animation.Death2]: { from: 40, to: 47 },
-    },
+  const sprites1 = [
+    [Sprite.Zombie1, 'sprites/zombies/zombie1.png'],
+    [Sprite.Zombie2, 'sprites/zombies/zombie2.png'],
+  ]
+
+  sprites1.forEach(([name, source]) => {
+    loadSprite(name, source, {
+      sliceX: 8,
+      sliceY: 6,
+      anims: {
+        [Animation.Idle]: { from: 0, to: 5 },
+        [Animation.Run]: { from: 8, to: 15 },
+        [Animation.Knocked]: { from: 16, to: 21 },
+        [Animation.Hit]: { from: 24, to: 26 },
+        [Animation.Death]: { from: 32, to: 39 },
+        [Animation.Death2]: { from: 40, to: 47 },
+      },
+    })
   })
 
-  loadSprite(Sprite.Human1, 'sprites/humans/human1.png', {
-    sliceX: 8,
-    sliceY: 5,
-    anims: {
-      [Animation.Idle]: { from: 0, to: 5 },
-      [Animation.Run]: { from: 8, to: 15 },
-      [Animation.Knocked]: { from: 16, to: 21 },
-      [Animation.Hit]: { from: 24, to: 26 },
-      [Animation.Death]: { from: 32, to: 39 },
-    },
+  const sprites2 = [
+    [Sprite.Human1, 'sprites/humans/human1.png'],
+    [Sprite.Zombie3, 'sprites/zombies/zombie3.png'],
+    [Sprite.Zombie4, 'sprites/zombies/zombie4.png'],
+  ]
+
+  sprites2.forEach(([name, source]) => {
+    loadSprite(name, source, {
+      sliceX: 8,
+      sliceY: 5,
+      anims: {
+        [Animation.Idle]: { from: 0, to: 5 },
+        [Animation.Run]: { from: 8, to: 15 },
+        [Animation.Knocked]: { from: 16, to: 21 },
+        [Animation.Hit]: { from: 24, to: 26 },
+        [Animation.Death]: { from: 32, to: 39 },
+      },
+    })
   })
 
   await loadSpriteAtlas('tilesets/interior.png', {
