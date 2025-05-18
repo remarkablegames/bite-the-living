4de5250571d0a6778fd4c555b2a7fa415fe332d7
Mark <mark@remarkablemark.org>
2025-05-17 22:01:44 -0400

    chore(scenes): set level with querystring during development

--- src/scenes/menu.ts
+++ src/scenes/menu.ts
@@ -2,10 +2,15 @@ import { Scene, Sound } from '../constants'
 import { addButton } from '../gameobjects'
 import { playSound } from '../helpers'
 import { startLevel } from '../levels'
+import { gameState } from '../states'
 
 scene(Scene.Menu, () => {
   const { x, y } = center()
   const margin = 20
+
+  if (import.meta.env.DEV) {
+    gameState.level = Number(new URLSearchParams(location.search).get('level'))
+  }
 
   add([
     text('Bite the Living', { size: 20, font: 'Monospace' }),
